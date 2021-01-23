e0b4edc0de21871fceb8520adb1fe219f2a86825
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-23 10:57:14 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('CONNECTED');
+
+const button = document.getElementById('main-btn');
+console.log(button);
+
+const selectButton = document.querySelector('#main-btn'); // # necessary bc you can target classes too
+console.log(selectButton);
