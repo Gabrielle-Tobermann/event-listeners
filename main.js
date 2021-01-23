abfc38c5a4b94db394bf994a306cfdd11dd1223d
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-23 11:08:56 -0600

    worked with callbacks

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log('CONNECTED');
+
+const sentence1 = (name) => {
+  return `My name is ${name}`;
+}
+
+const sentence2 = (name) => {
+  return `Hello ${name}`;
+}
+
+
+// TAKES A FUNCTION AS AN ARGUMENT
+const greeting = (name, cb) => {     // cb stands for callback function
+  return cb(name);                   
+}
+
+//    CONSOLE LOGGING THE RETURN STATEMENTS OF EACH CALLBACK
+console.log(greeting('Gabby', sentence1));
+console.log(greeting('Gabby', sentence2));
